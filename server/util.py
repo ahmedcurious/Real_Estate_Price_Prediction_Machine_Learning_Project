7f6559af54e718129748f9adc525d1f4f343075d
ahmedcurious <ahmed_naseem1@outlook.com>
2024-10-27 22:16:41 +0500

    location_named_displayed

--- server/util.py
+++ server/util.py
@@ -6,10 +6,11 @@ __locations = None
 __data_columns = None
 __model = None
 
-
 def get_location_names():
+    # Check if locations are loaded; if not, load them
+    if __locations is None:
+        load_saved_artifacts()
     return __locations
-
 
 def load_saved_artifacts():
     print("Loading Saved Artifacts ... Start")
@@ -17,14 +18,21 @@ def load_saved_artifacts():
     global __locations
     global __model
 
+    # Load columns and locations
     with open("./artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)["data_columns"]
-        __locations = __data_columns[3:]
+        location_temp = __data_columns[3:]
+        location_new=[]
+        for loc in location_temp:
+            new_loc = loc.split('_')[1]
+            location_new.append(new_loc)
+        __locations = location_new
+        # __locations = __data_columns[3:] # Assumes locations start at index 3
 
-    with open("./artifacts/banglore_home_prices_model.pickle", "+rb") as f:
+    # Load the model
+    with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f:
         __model = pickle.load(f)
     print("Loading Saved Artifacts ... Done")
-
 
 if __name__ == "__main__":
     load_saved_artifacts()
