10860d8bab15254afd6197582a1f037b53aa5e89
ahmedcurious <ahmed_naseem1@outlook.com>
2024-11-06 01:14:12 +0500

    hero_section_completed

--- client/house_prediction_model_frontend/src/components/Button.jsx
+++ client/house_prediction_model_frontend/src/components/Button.jsx
@@ -1,9 +1,18 @@
+import PropTypes from "prop-types";
+
 const Button = ({ text }) => {
   return (
-    <button className="bg-teal-300 text-white px-8 py-4 rounded-xl">
-        {text}
+    <button
+      className="bg-teal-300 w-fit font-montserrat font-semibold text-white text-xl px-8 py-4 rounded-xl shadow-md
+      hover:shadow-2xl hover:bg-teal-200"
+    >
+      {text}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+Button.propTypes = {
+  text: PropTypes.string,
+};
+
+export default Button;
--- client/house_prediction_model_frontend/tailwind.config.js
+++ client/house_prediction_model_frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}","./src/components/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: [ "Montserrat", "sans-serif"],
+        playfair_display: ["Playfair Display", "serif"],
+      },
+      backgroundImage: {
+        'custom-gradient': "linear-gradient(103deg, rgba(255, 255, 255, 0.052) -9.46%, rgba(255, 255, 255, 0.26) 110.33%)",
+      }
+    },
   },
   plugins: [],
 }
