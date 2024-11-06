68d737f3be2d5f3ecae7dd8dcdd705f3357828a0
ahmedcurious <ahmed_naseem1@outlook.com>
2024-11-06 16:54:32 +0500

    form_section_start

--- client/house_prediction_model_frontend/src/App.jsx
+++ client/house_prediction_model_frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Form from './components/Form'
 import Header from './components/Header'
 
 function App() {
 
   return (
-    <>
+    <div className="bg-teal-50">
       <Header />
-    </>
+      <Form />
+    </div>
   )
 }
 
--- client/house_prediction_model_frontend/src/components/Header.jsx
+++ client/house_prediction_model_frontend/src/components/Header.jsx
@@ -11,15 +11,19 @@ const Header = () => {
     >
       <div
         className="sm:mx-auto h-fit bg-custom-gradient border-solid border-2 border-slate-300 px-8 py-8 box-border
-        flex flex-col gap-12 rounded-3xl shadow-xl backdrop-blur"
+        flex flex-col gap-12 rounded-3xl shadow-xl backdrop-blur animate-pulse"
       >
         <div className="font-playfair_display font-bold text-white text-center text-6xl text-wrap leading-[4rem]">
-          Accurate Home Price<br />Predictions at Your Fingertips
+          Accurate Home Price
+          <br />
+          Predictions at Your Fingertips
         </div>
-        <div className="h-fit flex flex-col gap-6 items-center">
+        <div className="h-fit flex flex-col gap-6 items-center ">
           <div className="font-playfair_display font-normal text-white text-center text-4xl text-wrap leading-normal">
-            Curious about your home’s value or planning a property investment?<br />
-            Get data-driven price predictions tailored to your location,<br />
+            Curious about your home’s value or planning a property investment?
+            <br />
+            Get data-driven price predictions tailored to your location,
+            <br />
             square footage, and home features, all in real-time.
           </div>
           <Button text="Start Prediction!" />
