99ac1e4ce2563579f4c33b4905c31b59b40fcb66
ahmedcurious <ahmed_naseem1@outlook.com>
2024-11-19 14:21:56 +0500

    Modal Styles updated

--- client/house_prediction_model_frontend/src/components/PredictedPriceModal.jsx
+++ client/house_prediction_model_frontend/src/components/PredictedPriceModal.jsx
@@ -3,29 +3,65 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
-  Button as MuiButton,
 } from "@mui/material";
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const PredictedPriceModal = ({ open, price, onClose }) => {
   const priceInLakhs = price * 100000;
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Predicted Price</DialogTitle>
-      <DialogContent>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      sx={{
+        "& .MuiDialog-paper": {
+          width: "500px", // Optional: Adjust width
+          borderRadius: "10px", // Optional: Rounded corners
+          padding: "20px", // Padding around the content
+          border: "2px solid #74ebd4 !important", // Adding !important to ensure the border is applied
+          boxShadow: "none !important", // Remove default box-shadow that might interfere with border
+          overflow: "visible", // Ensure that the border is not clipped
+        },
+      }}
+    >
+      <DialogTitle
+        sx={{
+          fontSize: "36px",
+          fontWeight: "bold",
+          fontFamily: ["Playfair Display", "serif"],
+          color: "#74ebd4",
+          paddingBottom: "16px",
+          textAlign: "center",
+        }}
+      >
+        Predicted Price
+      </DialogTitle>
+
+      <DialogContent
+        sx={{
+          textAlign: "center",
+          paddingTop: "16px",
+          paddingBottom: "16px",
+        }}
+      >
         <div className="text-center">
-          <h3 className="text-xl font-bold">
+          <h3 className="text-xl font-montserrat font-light">
             Predicted Price for your property:
           </h3>
-          <p className="mt-4 text-3xl font-semibold">
+          <p className="mt-4 text-3xl font-montserrat font-semibold text-slate-500">
             {price ? `₹${priceInLakhs.toLocaleString()}` : "N/A"}
           </p>
         </div>
       </DialogContent>
-      <DialogActions>
-        <MuiButton onClick={onClose} color="primary">
-          Close
-        </MuiButton>
+
+      <DialogActions
+        sx={{
+          paddingTop: "10px",
+          paddingBottom: "10px",
+          justifyContent: "center",
+        }}
+      >
+        <Button text="Close" onClick={onClose} />
       </DialogActions>
     </Dialog>
   );
